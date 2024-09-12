ffe56a5d45ab2abcba06c465f88e54e5f7a1b1d9
Rafael Lorente Padovan <112407703+Refex0110@users.noreply.github.com>
2024-09-12 15:55:16 -0300

    Add files via upload

---
+++ ClassesIphone/Iphone/conteudo-iphone/src/AparelhoTelefonico.java
@@ -0,0 +1,5 @@
+public interface AparelhoTelefonico {
+    public void ligar(String numero);
+    public boolean atender();
+    public void iniciarCorreioVoz(String mensagem, String destinatario);
+}
---
+++ ClassesIphone/Iphone/conteudo-iphone/src/IPhone.java
@@ -0,0 +1,45 @@
+public class IPhone implements NavegadorInternet, ReprodutorMusical, AparelhoTelefonico{
+    @Override
+    public void ligar(String numero){
+    }
+    
+    @Override
+    public void atender() {
+        //Implementação aqui
+    }
+
+    @Override
+    public void iniciarCorreioVoz(String mensagem, String destinatario) {
+        //Implementação aqui
+    }
+
+    @Override
+    public void exibirPagina(String url) {
+        //Implementação aqui
+    }
+
+    @Override
+    public void adicionarNovaAba(String url) {
+        //Implementação aqui
+    }
+
+    @Override
+    public void atualizarPagina() {
+        //Implementação aqui
+    }
+
+    @Override
+    public void tocar() {
+        //Implementação aqui
+    }
+
+    @Override
+    public void pausar() {
+        //Implementação aqui
+    }
+
+    @Override
+    public void selecionarMusica() {
+        //Implementação aqui
+    }
+}
